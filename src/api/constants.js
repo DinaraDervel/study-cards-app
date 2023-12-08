fa1db1961c34e3181416974f5fcd242fc3c8ce40
DinaraDervel <dinaradervel@gmail.com>
2023-12-08 11:10:41 -0600

    change http to https

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,6 +1,6 @@
 export const URLS = {
-    GET_ALL_WORDS: 'http://itgirlschool.justmakeit.ru/api/words',
-    UPDATE_WORD: 'http://itgirlschool.justmakeit.ru/api/words/:id/update',
-    ADD_WORD: 'http://itgirlschool.justmakeit.ru/api/words/add',
-    DELETE_WORD: 'http://itgirlschool.justmakeit.ru/api/words/:id/delete'
+    GET_ALL_WORDS: 'https://itgirlschool.justmakeit.ru/api/words',
+    UPDATE_WORD: 'https://itgirlschool.justmakeit.ru/api/words/:id/update',
+    ADD_WORD: 'https://itgirlschool.justmakeit.ru/api/words/add',
+    DELETE_WORD: 'https://itgirlschool.justmakeit.ru/api/words/:id/delete'
 }
